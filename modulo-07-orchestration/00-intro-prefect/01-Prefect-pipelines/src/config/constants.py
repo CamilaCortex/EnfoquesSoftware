7cdd5d6d765b5fa79880efe24e862a40115e8a95
CamilaCortex <mariadurangoai@gmail.com>
2026-06-03 13:01:50 -0500

    feat: include deploy module 08

---
+++ modulo-07-orchestration/00-intro-prefect/01-Prefect-pipelines/src/config/constants.py
@@ -0,0 +1,27 @@
+"""
+Pipeline constants and configuration values.
+"""
+
+# Data configuration
+DEFAULT_YEAR = 2025
+DEFAULT_MONTH = 1
+MIN_RECORDS = 1000
+NULL_THRESHOLD = 10.0
+
+# Model configuration
+OPTUNA_TRIALS = 20
+NUM_BOOST_ROUNDS = 30
+EARLY_STOPPING_ROUNDS = 50
+
+# MLflow configuration
+MLFLOW_EXPERIMENT_NAME = "nyc-taxi-experiment-prefect"
+MLFLOW_DEFAULT_URI = "sqlite:///mlflow.db"
+
+# Data quality thresholds
+MIN_DURATION = 1
+MAX_DURATION = 60
+
+# Feature configuration
+CATEGORICAL_FEATURES = ['PULocationID', 'DOLocationID']  # Columnas originales del dataset NYC Taxi
+TARGET_COLUMN = 'duration'
+
